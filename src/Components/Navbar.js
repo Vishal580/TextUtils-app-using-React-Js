d5e4e44905929e4c1a71e955dd15f44a9b5f1fc1
Vishal580 <R20200530.VISHAL@cse.reva.edu.in>
2024-11-09 23:57:00 +0530

    Props and PropTypes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">TextUtils</a>
+          <a className="navbar-brand" href="/">{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -15,7 +16,7 @@ export default function Navbar() {
                 <a className="nav-link active" aria-current="page" href="/">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">Link</a>
+                <a className="nav-link" href="/">{props.about}</a>
               </li>
             </ul>
             <form className="d-flex" role="search">
@@ -27,4 +28,14 @@ export default function Navbar() {
       </nav>
     </div>
   )
+}
+
+Navbar.prototype = {
+    title: PropTypes.string.isRequired,
+    about: PropTypes.string.isRequired
+}
+
+Navbar.defaultProps = {
+    title: "Home",
+    about: "About Us"
 }
