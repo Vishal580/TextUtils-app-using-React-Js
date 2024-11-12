f024bfdc5090efd794ba065e61da6c5461cf58ea
Vishal580 <R20200530.VISHAL@cse.reva.edu.in>
2024-11-12 18:10:32 +0530

    Hosting app on GitHub

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { useState } from 'react';
 import './App.css';
-import About from './Components/About';
+// import About from './Components/About';
 import Navbar from "./Components/Navbar";
 import TextForm from './Components/TextForm';
 import Alert from './Components/Alert';
-import {
-  BrowserRouter as Router,
-  //Switch,
-  Route,
-  //Link,
-  Routes
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   //Switch,
+//   Route,
+//   //Link,
+//   Routes
+// } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
@@ -73,16 +73,17 @@ function App() {
 
   return (
     <>
-    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    {/* <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}> */}
       <Navbar title="TextUtils" about="About" mode={mode} toggleMode={toggleMode} brownMode={brownMode} skyMode={skyMode}/>
       <Alert alert={alert}/>
       <div className="container my-3">
-      <Routes>
+      {/* <Routes>
           <Route exact path="/about" element={<About/>}></Route>      
           <Route exact path="/" element = {<TextForm headline="Enter your text to anlyze below" mode={mode} showAlert={showAlert}/>}></Route>
-      </Routes>
+      </Routes> */}
+      <TextForm headline="Enter your text to anlyze below" mode={mode} showAlert={showAlert}/>
       </div>
-    </Router>
+    {/* </Router> */}
     </>
   );
 }
